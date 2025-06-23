157ec3d1aa40c2dba8f8c70963336631ab709115
RAGINIVENKET03 <raginivenket.01@gmail.com>
2025-06-23 09:06:04 +0530

    Fixed the return status in Recipe Controller and bug in the User login in User Controller

--- backend/controller/recipeController.js
+++ backend/controller/recipeController.js
@@ -60,7 +60,7 @@ const addRecipe=async(req,res)=>
     catch(error)
     {
         console.error('Error in adding the recipe:',error.message);
-        res.status(400).send({message:"Error in adding the recipe"})
+        return res.status(400).send({message:"Error in adding the recipe"})
     }
 }
 const updateRecipeById=async(req,res)=>
@@ -71,13 +71,13 @@ const updateRecipeById=async(req,res)=>
         let photo=req.file?.filename ? req.file?.filemane: recipe.photo
         const updateRecipe=await Recipe.findByIdAndUpdate(id,{...req.body,photo},{new:true});
         if(!updateRecipe){
-            res.status(404).send({message:"Error in updating the Recipe"});
+            return res.status(404).send({message:"Error in updating the Recipe"});
         }
-        res.status(200).send({updateRecipe:updateRecipe});
+        return res.status(200).send({updateRecipe:updateRecipe});
     }
     catch(error){
         console.error("Error in updating the recipe:",error.message);
-        res.status(400).send({message:"Error in updating the recipe"});
+        return res.status(400).send({message:"Error in updating the recipe"});
     }
 }
 const deleteRecipeById=async(req,res)=>
@@ -87,13 +87,13 @@ const deleteRecipeById=async(req,res)=>
         const deleteRecipe=await Recipe.findByIdAndDelete(id);
         if(!deleteRecipe)
         {
-            res.status(404).send({message:"Recipe not found"});
+            return res.status(404).send({message:"Recipe not found"});
         }
-        res.status(200).send({message:"Recipe deleted successfully"});
+        return res.status(200).send({message:"Recipe deleted successfully"});
     }
     catch(error){
         console.error("Error in deleting the recipe");
-        res.status(400).send({message:"Error in deleting the recipe"});
+        return res.status(400).send({message:"Error in deleting the recipe"});
     }
 }
 module.exports={getAllRecipes,getRecipeById,addRecipe,updateRecipeById,deleteRecipeById,upload}
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -23,27 +23,28 @@ exports.userSignUp = async (req, res) => {
 
     const token = jwt.sign({ email, id: newUser._id }, process.env.SECRET_KEY);
     return res.status(200).send({ token, user: newUser });
-  } catch (err) {
-    console.error("Signup Error:", err); // This will now log the MongoDB error
+  } 
+  catch (error) {
+    console.error("Signup Error:", error); // This will now log the MongoDB error
     return res.status(500).send({ error: "Internal Server Error" });
   }
 };
 exports.userLogin=async(req,res)=>{
     const {email,password}=req.body;
-        if(!email ||!password){
-            return res.status(400).send({error:"email or password cannot be empty"})
-        }
-        let user=await User.findOne({email})
-        if(user && bcrypt.compare(password,user.password)){
-            let token=jwt.sign({email,id:user._id},process.env.SECRET_KEY,{ expiresIn: "3d" })
-            return res.status(200).send({token,user})
-        }
-        else{
-            return res.status(400).send({error:"Invalid credentials"})
-        }
+    if(!email ||!password){
+        return res.status(400).send({error:"email or password cannot be empty"})
+    }
+    let user=await User.findOne({email})
+    if(user && await bcrypt.compare(password,user.password)){
+        let token=jwt.sign({email,id:user._id},process.env.SECRET_KEY,{ expiresIn: "3d" })
+        return res.status(200).send({token,user})
+    }
+    else{
+        return res.status(400).send({error:"Invalid credentials."})
+    }
 }
 exports.getUser=async(req,res)=>{
     const id=req.params.id;
-        const user=await User.findById(id);
-        return res.status(200).send({email:user.email})
+    const user=await User.findById(id);
+    return res.status(200).send({email:user.email})
 }
